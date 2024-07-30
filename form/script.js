206a32a3134a45dad4ee8086efc1dc7bf3b499b5
himanshudhaked9 <139742927+himanshudhaked9@users.noreply.github.com>
2024-07-30 12:45:02 +0530

    Add files via upload

---
+++ form/script.js
@@ -0,0 +1,44 @@
+var tl = gsap.timeline()
+tl.from(".heading",{
+  opacity:0,
+  y:20,
+  duration:1,
+  delay:0.5
+
+})
+tl.from(".inputbox",{
+  opacity:0,
+  y:20,
+  duration:1,
+  stagger:0.3
+
+})
+tl.from(".gen-title",{
+  opacity:0,
+  y:20,
+  duration:1,
+  
+
+})
+tl.from(".category",{
+  opacity:0,
+  y:20,
+  duration:1,
+  stagger:0.3
+  
+
+})
+tl.from(".terms",{
+  opacity:0,
+  y:20,
+  duration:1,
+  
+
+})
+tl.from(".btn",{
+  opacity:0,
+  y:20,
+  duration:1,
+  
+
+})
